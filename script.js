c271a7aac705a3c4855f0c82c094a401c65f2fcd
Righteous1ice <hoplabradley@gmail.com>
2023-05-29 20:51:04 +0100

    Finished

--- script.js
+++ script.js
@@ -35,44 +35,88 @@ function addBookToLibrary(title, author, pages, status){
 }
 
 
+
 ////////// CARD DISPLAY \\\\\\\\\\
 
 function displayCard(event) {
     event.preventDefault();
     var cardContainer = document.getElementById('cardContainer');
-
-
-    if (myLibrary.length === 0) {
-        cardContainer.innerHTML = ''; // Remove any existing cards
-        return;
-    }
-
-
-
-
-
-
     cardContainer.innerHTML = '';
-
-   
 
     for (var i = 0; i < myLibrary.length; i++) {
         var book = myLibrary[i];
 
-
-
         var card = document.createElement('div');
         card.className = 'card';
 
-        var cardContent = '<h2>' + book.title + '</h2>' +
-            '<p><strong>Author:</strong> ' + book.author + '</p>' +
-            '<p><strong>Page Count:</strong> ' + book.pages + '</p>' +
-            '<p><strong>Reading Status:</strong> ' + book.status + '</p>';
+        var cardContent =
+            '<h2>' +
+            book.title +
+            '</h2>' +
+            '<p><strong>Author:</strong> ' +
+            book.author +
+            '</p>' +
+            '<p><strong>Page Count:</strong> ' +
+            book.pages +
+            '</p>' +
+            '<p><strong>Reading Status:</strong> ' +
+            book.status +
+            '</p>' +
+            '<button class="edit-btn" id="edit-btn-' + i + '">Edit</button>' +
+            '<button class="delete-btn" id="delete-btn-' +
+            i +
+            '">Delete</button>';
 
         card.innerHTML = cardContent;
         cardContainer.appendChild(card);
+
+        (function (index) {
+            var deleteButton = card.querySelector('#delete-btn-' + index);
+            deleteButton.addEventListener('click', function () {
+                deleteBook(index);
+            });
+        })(i);
+
+        var editButton = card.querySelector('#edit-btn-' + i);
+        editButton.addEventListener('click', function () {
+            var index = Array.from(cardContainer.querySelectorAll('.edit-btn')).indexOf(this);
+            editReadingStatus(index);
+        });
     }
 
     document.getElementById('bookForm').reset();
 }
 
+
+  ////////// DELETE BOOK \\\\\\\\\\
+  
+  function deleteBook(index) {
+    myLibrary.splice(index, 1);
+    displayCard(event);
+  }
+  
+  ////////// EDIT STATUS \\\\\\\\\\
+
+  function editReadingStatus(index) {
+    var newStatus = prompt('Enter the new reading status (Read, In Progress, To Be Read):');
+  
+   
+    if (newStatus && ['Read', 'In Progress', 'To Be Read'].includes(newStatus)) {
+      
+      myLibrary[index].status = newStatus;
+  
+     
+      var card = document.getElementsByClassName('card')[index];
+      var statusElement = Array.from(card.getElementsByTagName('p')).find(function (element) {
+        return element.textContent.includes('Reading Status:');
+      });
+  
+      if (statusElement) {
+        var statusTextNode = statusElement.childNodes[1];
+        statusTextNode.textContent = ' ' + newStatus;
+      }
+    } else {
+      alert('Invalid reading status. Please enter Read, In Progress, or To Be Read.');
+    }
+  }
+  
